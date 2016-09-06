12c153c5af8a51abcc59f569507db3b5055dd2bd
aledaas <aledaas@gmail.com>
2016-09-06 10:04:50 -0300

    TNRE-834

--- src/plugins/facebook/plugin.jsx
+++
@@ -1,9 +0,0 @@
-import FacebookButton from "./FacebookButton";
-import FacebookBlock from "./FacebookBlock";
-
-export default {
-  type: "facebook",
-  buttonComponent: FacebookButton,
-  blockComponent: FacebookBlock,
-  options:{displayOptions:[]}
-};
