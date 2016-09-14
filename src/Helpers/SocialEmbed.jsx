9fd0d4940392b9b2319aa3ab7b5ccf997336a5f6
Gaston <briones.gaston@gmail.com>
2016-09-14 16:29:22 -0300

    added validate url for tag script

--- src/Helpers/SocialEmbed.jsx
+++ src/Helpers/SocialEmbed.jsx
@@ -1,6 +1,6 @@
-import domify from 'domify'
+import domify from 'domify';
 import S from 'string';
-import embedsList from './EmbedConfig'
+import embedsList from './EmbedConfig';
 
 export default class socialEmbed {
 
@@ -16,7 +16,7 @@ export default class socialEmbed {
         return [];
     }
 
-    static inspectUrl(url, compare) {
+    static validateUrl(url, compare) {
         if (S(url).startsWith('http://', 'https://')) {
             if (S(url).contains(compare)) {
                 return true;
@@ -24,6 +24,22 @@ export default class socialEmbed {
         }
         return false;
     }
+
+    static generateValidUrl(url) {
+
+        if (S(url).startsWith('http://', 'https://')) {
+            return url;
+        } else {
+            if (S(url).startsWith('//')) {
+                return S(url).replaceAll('//', 'https://').s
+            } else if (S(url).startsWith('./')) {
+                return S(url).replaceAll('./', 'https://').s
+            } else {
+                this.displayError(url + ' no es una url válida');
+            }
+        }
+    }
+
 
     static matchSocialEmbed(string) {
         var element = domify(this.cleanHtml(string));
@@ -33,44 +49,63 @@ export default class socialEmbed {
         for (let key of Object.keys(embedsList)) {
             const embed = embedsList[key];
 
-            if (element.nodeName == embed.domObj) {
-                if (element.nodeType == isXMLNode) {
-                    switch (embed.domObj) {
-                        case 'IFRAME':
-                            if (S(element.getAttribute(embed.compare.attr)).contains(embed.compare.value)) {
-                                this.displayError(embed.blockName + ' es un embed valido');
-                                return embed.blockName;
-                            }
-                            break;
+            if (element.nodeName == embed.domObj && element.nodeType == isXMLNode) {
+                switch (embed.domObj) {
+                    case 'IFRAME':
+                        if (S(element.getAttribute(embed.compare.attr)).contains(embed.compare.value)) {
+                            this.displayError(embed.blockName + ' es un embed valido');
+                            return embed.blockName;
+                        }
+                        break;
 
-                        default:
-                            if (element.getAttribute(embed.compare.attr) == embed.compare.value) {
-                                this.displayError(embed.blockName + ' es un embed valido');
-                                return embed.blockName;
-                            }
-                            break;
-                    }
-
-                } else if (element.nodeType == isStringNode) {
-                    if (this.inspectUrl(string, embed.compare.value)) {
-                        this.displayError(embed.blockName + ' es un embed valido');
-                        return embed.blockName
-                    }
+                    default:
+                        if (element.getAttribute(embed.compare.attr) == embed.compare.value) {
+                            this.displayError(embed.blockName + ' es un embed valido');
+                            return embed.blockName;
+                        }
+                        break;
                 }
+
+            } else if (element.nodeType == isStringNode) {
+                console.log('URL --> ',this.generateValidUrl(string));
+                /*if (this.validateUrl(string, embed.compare.value)) {
+                 this.displayError(embed.blockName + ' es un embed valido');
+                 return embed.blockName
+                 }*/
             }
         }
-        this.displayError();
+        //this.displayError();
+    }
+
+    static sanitizeScripsUrl(scriptArr){
+        let validScripts = [];
+
+        if(scriptArr.length > 0){
+            scriptArr.forEach(function (val) {
+                const domObj = domify(val);
+                const attrValue = domObj.getAttribute('src');
+                const newAttrValue = this.generateValidUrl(attrValue);
+                const newObj = domObj.src = newAttrValue;
+                validScripts.push(newObj);
+            }.bind(this));
+            return validScripts
+        }
+
+        return validScripts;
     }
 
     static createDataObject(data) {
-        const content = {content: this.cleanHtml(data), script: this.findTagScript(data)};
+        const scripts = this.sanitizeScripsUrl(this.findTagScript(data));
+        console.log('scripts --> ',scripts);
+
+        const content = {content: this.cleanHtml(data), script:scripts };
         const type = this.matchSocialEmbed(data);
 
         return {data: content, type: type};
     }
 
-    static displayError (error = 'error'){
-        console.log('Mensaje de error --> ',error);
+    static displayError(error = 'error') {
+        console.log('Mensaje de error --> ', error);
     }
 }
 
