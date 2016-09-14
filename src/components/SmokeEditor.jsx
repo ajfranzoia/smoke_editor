dc59a7cdeeb1697d2f4ed89c928d035c2b9e3ed8
leanazulyoro <leanazulyoro@gmail.com>
2016-09-14 16:27:14 -0300

    added renderMap to set paragraphs as p

--- src/components/SmokeEditor.jsx
+++ src/components/SmokeEditor.jsx
@@ -1,4 +1,9 @@
-import {MegadraftEditor} from "megadraft";
+import React, {Component} from "react";
+import {MegadraftEditor, Toolbar, Sidebar} from "megadraft";
+import {DefaultDraftBlockRenderMap, Editor} from 'draft-js';
+import Immutable from "immutable";
+const {Map} = Immutable;
+
 
 
 export default class SmokeEditor extends MegadraftEditor {
@@ -16,4 +21,58 @@ export default class SmokeEditor extends MegadraftEditor {
         });
         return style;
     }
+
+    BlockRenderingMap = () => {
+
+        const customBlockRendering = Map({
+            'paragraph': {
+                element: 'p',
+            },
+            'unstyled': {
+                element: 'p',
+            }
+        });
+        return DefaultDraftBlockRenderMap.merge(customBlockRendering);
+    }
+
+
+    render() {
+        const {editorState, stripPastedStyles, spellCheck} = this.props;
+        const plugins = this.plugins;
+
+        return (
+            <div className="megadraft">
+                <div
+                    className="megadraft-editor"
+                    id="megadraft-editor"
+                    ref="editor">
+                    <Sidebar
+                        plugins={plugins}
+                        editorState={editorState}
+                        onChange={this.onChange}/>
+                    <Editor
+                        blockRenderMap={this.BlockRenderingMap()}
+                        readOnly={this.state.readOnly}
+                        plugins={plugins}
+                        blockRendererFn={this.mediaBlockRenderer}
+                        blockStyleFn={this.blockStyleFn}
+                        onTab={this.onTab}
+                        handleKeyCommand={this.handleKeyCommand}
+                        handleReturn={this.handleReturn}
+                        stripPastedStyles={stripPastedStyles}
+                        spellCheck={spellCheck}
+                        keyBindingFn={this.externalKeyBindings}
+                        editorState={editorState}
+                        placeholder={this.props.placeholder}
+                        onChange={this.onChange} />
+                    <Toolbar
+                        editor={this.refs.editor}
+                        editorState={editorState}
+                        onChange={this.onChange}
+                        actions={this.actions}/>
+                </div>
+            </div>
+        );
+    }
+
 }
