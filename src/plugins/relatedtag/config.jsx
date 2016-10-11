897133f91b1b89fa7074f26416c54dd0d3681023
leanazulyoro <leanazulyoro@gmail.com>
2016-10-11 17:16:14 -0300

    changed config urls to be relative

--- src/plugins/relatedtag/config.jsx
+++ src/plugins/relatedtag/config.jsx
@@ -1,4 +1,4 @@
 export default {
-    contentTagPeopleUrl:        "http://local.next.tn.com.ar:8080/smoke-editor/autocomplete/tag-people/",
-    latestThreeArticlesByTid:   "http://local.next.tn.com.ar:8080/smoke-editor/get/latest-three-articles-by-tid/"
+    contentTagPeopleUrl:        "/smoke-editor/autocomplete/tag-people/",
+    latestThreeArticlesByTid:   "/smoke-editor/get/latest-three-articles-by-tid/"
 };
