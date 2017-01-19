2e428e6b1a4adb0a23508021fb138352695799f2
Gustavo Javier Aguirre <gjagui@gmail.com>
2017-01-19 10:23:43 -0300

    Fixed: Tag/Personajes/Embed

--- src/Helpers/SocialEmbed.jsx
+++ src/Helpers/SocialEmbed.jsx
@@ -5,12 +5,12 @@ import embedsList from './EmbedConfig';
 export default class socialEmbed {
 
     static cleanHtml(string) {
-        const regex = /<(\w+)[^>]*>/g;
+        const regex = /<(\w+)[^>]*>.*<\/(\w+)>/g;
         const str =  S(string).stripTags('p','a','time').s;
 
         if (regex.test(str)) {
-            return str.match(regex)[0];
-        }else{
+            return str.match(regex);
+        } else {
             return "";
         }
     }
@@ -62,31 +62,35 @@ export default class socialEmbed {
 
 
     static matchSocialEmbed(string) {
-        
+
         var cleanedHtml = this.cleanHtml(string);
-        var element = domify(cleanedHtml);
 
-        for (let key of Object.keys(embedsList)) {
-            const embed = embedsList[key];
-            if (element.nodeName == embed.domObj) {
-                switch (embed.domObj) {
-                    case 'IFRAME':
-                        if (S(element.getAttribute(embed.compare.attr)).contains(embed.compare.value)) {
-                            return embed.blockName;
-                        }
-                        break;
+        let embedType = false;
+        cleanedHtml.forEach(function (cleanedElement) {
+            var element = domify(cleanedElement);
 
-                    default:
-                        if (element.getAttribute(embed.compare.attr) == embed.compare.value) {
-                            return embed.blockName;
-                        }
-                        break;
+            for (let key of Object.keys(embedsList)) {
+                const embed = embedsList[key];
+                if (element.nodeName == embed.domObj) {
+                    switch (embed.domObj) {
+                        case 'IFRAME':
+                            if (S(element.getAttribute(embed.compare.attr)).contains(embed.compare.value)) {
+                                embedType = embed.blockName;
+                            }
+                            break;
+
+                        default:
+                            if (element.getAttribute(embed.compare.attr) == embed.compare.value) {
+                                embedType = embed.blockName;
+                            }
+                            break;
+                    }
+
                 }
-
             }
-        }
+        }.bind(embedType));
 
-        return false;
+        return embedType;
     }
 
     static sanitizeScripsUrl(scriptArr){
@@ -128,4 +132,3 @@ export default class socialEmbed {
         return error;
     }
 }
-
